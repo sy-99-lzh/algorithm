f4812d81092707439a9edbc703420f5687690797
songyi-- <108929478+sy-99-lzh@users.noreply.github.com>
2023-06-12 13:17:19 +0800

    Document submitted

--- ITCC/ITCC_ALG.m
+++
@@ -1,6 +0,0 @@
-function [row_cluster_label, column_cluster_label, time] = ITCC_ALG(data, row_class_number, column_class_number)
-    %% Running
-    tic
-    [row_cluster_label, column_cluster_label] = ITL(data, row_class_number, column_class_number);
-    time = toc;
-end
