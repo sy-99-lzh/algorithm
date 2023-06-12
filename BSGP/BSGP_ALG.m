d57d2aee9ab66c5a63126beb7d8a2b76c6d129bf
songyi-- <108929478+sy-99-lzh@users.noreply.github.com>
2023-06-12 13:11:18 +0800

    Delete BSGP_ALG.m

--- BSGP/BSGP_ALG.m
+++
@@ -1,6 +0,0 @@
-function [row_cluster_label, column_cluster_label, time] = BSGP_ALG(data, row_class_number)
-    %% running
-    tic
-    [row_cluster_label, column_cluster_label] = SpectralCoClustering(data, row_class_number);
-    time = toc;
-end
