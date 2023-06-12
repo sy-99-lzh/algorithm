1e852e14fc9f67b26eec226b3d3f1ea62d61110f
songyi-- <108929478+sy-99-lzh@users.noreply.github.com>
2023-06-12 13:18:57 +0800

    Document submitted

--- SOBG/SOBG_ALG.m
+++
@@ -1,6 +0,0 @@
-function [row_cluster_label, column_cluster_label, time] = SOBG_ALG(data, row_class_number)
-    %% Running
-    tic
-    [row_cluster_label, column_cluster_label, S] = coclustering_bipartite_fast(data, row_class_number);
-    time = toc;
-end  
